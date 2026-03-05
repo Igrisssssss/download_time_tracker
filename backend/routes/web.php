3dab56d599e7513e90a5392ca7d3e82a45319503
Aayush8780 <ayushborwal9991@gmail.com>
2026-03-05 12:38:06 +0530

    Replace welcome page with API health response on root route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'carevance-hrms-api',
+        'message' => 'API is running',
+    ]);
 });
